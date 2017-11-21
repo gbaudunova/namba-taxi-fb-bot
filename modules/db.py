006e50b9b514754a021ac92455c3ccd790d1e830
Gulbustan <gbaudunova@gmail.com>
2017-11-21 10:37:51 +0600

    Added handler that inserts fares into db

--- modules/db.py
+++ modules/db.py
@@ -4,13 +4,20 @@ import sqlite3
 def insertPhoneNumbers(data):
     db = sqlite3.connect('./phone_numbers.db')
     c = db.cursor()
-    #c.execute("CREATE TABLE contacts (Numbers INTEGER)")
+    # c.execute("CREATE TABLE contacts (Numbers INTEGER)")
     variable = data['entry'][0]['messaging'][0]['message']['text']
     print(variable)
-    c.execute("INSERT INTO contacts VALUES (?)", (variable, ))
+    c.execute("INSERT INTO contacts VALUES (?)", (variable,))
     db.commit()
 
 
+def insertFares(data):
+    db1 = sqlite3.connect('./phone_numbers.db')
+    conn = db1.cursor()
+    callback = data['entry'][0]['messaging'][0]['postback']['payload']
+    print(callback)
+    conn.execute("INSERT INTO fares VALUES (?)", (callback,))
+    db1.commit()
 
 
 
@@ -26,11 +33,5 @@ def insertPhoneNumbers(data):
 
 
 
-
-
-
-
-
-
-
-
+    # db.close()
+    # c.close()
