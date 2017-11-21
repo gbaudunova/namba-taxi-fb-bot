0123b121f5c3dd4b54115f57f8dc26395f9d0f89
Gulbustan <gbaudunova@gmail.com>
2017-11-21 10:36:51 +0600

    Added fares buttons

--- modules/needFares.py
+++ modules/needFares.py
@@ -1,53 +1,60 @@
 import json
 import requests
 from .sekret import *
+from .db import insertFares
 
 
-# def sendFares(sender_id):
-#     params = {
-#         "access_token": PAGE_ACCESS_TOKEN
-#     }
-#     data = json.dumps({
-#         "recipient": {
-#             "id": sender_id
-#         },
-#         "message": {
-#             "attachment": {
-#                 "type": "template",
-#                 "payload": {
-#                     "template_type": "button",
-#                     "text": "Нажмите на кнопку для сохранения вашего номера",
-#                     "buttons": [
-#                         {
-#                             "type": "postback",
-#                             "title": "Стандарт",
-#                             "payload": "standard-fare"
-#                         },
-#                         {
-#                             "type": "postback",
-#                             "title": "Минивэн",
-#                             "payload": "minivan-fare"
-#                         },
-#                         {
-#                             "type": "postback",
-#                             "title": "Комфорт",
-#                             "payload": "comfort-fare"
-#                         },
-#                         {
-#                             "type": "postback",
-#                             "title": "Байк+",
-#                             "payload": "bike-fare"
-#                         },
-#                         {
-#                             "type": "postback",
-#                             "title": "Портер",
-#                             "payload": "porter-fare"
-#                         }
-#                     ]
-#                 }
-#             }
-#         }
-#     })
-#     headers = {'Content-type': 'application/json'}
-#     r = requests.post(URL, data=data, params=params, headers=headers)
-#
+def send_fares(sender_id):
+    params = {"access_token": PAGE_ACCESS_TOKEN}
+    data = json.dumps({
+        "recipient": {
+            "id": sender_id
+        },
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [
+                        {
+                            "title": "Проведите влево/вправо для получения дополнительных тарифов.",
+                            "buttons": [
+                                {
+                                    "type": "postback",
+                                    "title": "Стандарт",
+                                    "payload": "standard-fare"
+                                },
+                                {
+                                    "type": "postback",
+                                    "title": "Минивэн",
+                                    "payload": "minivan-fare"
+                                },
+                                {
+                                    "type": "postback",
+                                    "title": "Комфорт",
+                                    "payload": "comfort-fare"
+                                }
+                                ]
+                        },
+                        {
+                            "title": "Проведите влево/вправо для получения дополнительных тарифов.",
+                            "buttons": [
+                                {
+                                    "type": "postback",
+                                    "title": "Байк+",
+                                    "payload": "bike-fare"
+                                },
+                                {
+                                    "type": "postback",
+                                    "title": "Портер",
+                                    "payload": "porter-fare"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    })
+    headers = {'Content-type': 'application/json'}
+    r = requests.post(URL, data=data, params=params, headers=headers)
