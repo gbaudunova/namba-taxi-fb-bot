41657d458c2688e3ded96c9718a2b35668b68799
Gulbustan <gbaudunova@gmail.com>
2017-11-24 20:54:57 +0600

    No changes

--- modules/orderCreation.py
+++ modules/orderCreation.py
@@ -5,8 +5,6 @@ import uuid
 def CreateOrder():
     db = sqlite3.connect('NambaTaxiBot.db')
     c = db.cursor()
-    # c.execute("SELECT last_insert_rowid() FROM phoneNumbers")
-    # c.execute("SELECT max(Numbers) FROM phoneNumbers")
     c.execute("SELECT * FROM phoneNumbers WHERE id=(SELECT max(id) FROM phoneNumbers);")
     phoneNumber = c.fetchone()
     print(phoneNumber)
@@ -20,4 +18,3 @@ def CreateOrder():
     c.execute("INSERT INTO CreateOrder (phoneNumber, fare, address, orderId) VALUES (?, ?, ?, ?)", (str(phoneNumber[1]), str(fare[1]), str(address[1]), orderId, ))
     db.commit()
 
-# print(str(uuid.uuid4().fields[0])[:8])
