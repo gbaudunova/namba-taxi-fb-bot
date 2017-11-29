7da2b6894973f8d2718bf08480a4c88d0c4cca7c
Gulbustan <gbaudunova@gmail.com>
2017-11-30 00:57:57 +0600

    Add some modifications

--- modules/get_data.py
+++ modules/get_data.py
@@ -1,18 +1,19 @@
 import sqlite3
+from api.requests import create_order
 
 
 def get_data():
     db = sqlite3.connect('NambaTaxiBot.db')
     c = db.cursor()
-    c.execute("SELECT * FROM phoneNumbers WHERE id=(SELECT max(id) FROM phoneNumbers);")
-    PHONE_NUMBER = c.fetchone()
-    return PHONE_NUMBER
-    c.execute("SELECT * FROM Fares WHERE id=(SELECT max(id) FROM Fares);")
-    FARE = c.fetchone()
-    return FARE
-    c.execute("SELECT * FROM Address WHERE id=(SELECT max(id) FROM Address);")
-    ADDRESS = c.fetchone()
-    return ADDRESS
+    c.execute("SELECT * FROM phone_number WHERE id=(SELECT max(id) FROM phone_number);")
+    phone_number = c.fetchone()
+    print(phone_number[1])
+    c.execute("SELECT * FROM fare WHERE id=(SELECT max(id) FROM fare);")
+    fare = c.fetchone()
+    print(fare[1])
+    c.execute("SELECT * FROM address WHERE id=(SELECT max(id) FROM address);")
+    address = c.fetchone()
+    print(address[1])
     db.commit()
     create_order(phone_number, fare, address)
 
@@ -34,19 +35,7 @@ def get_data():
 
 
 
-# def CreateOrder():
-#     db = sqlite3.connect('NambaTaxiBot.db')
-#     c = db.cursor()
-#     c.execute("SELECT * FROM phoneNumbers WHERE id=(SELECT max(id) FROM phoneNumbers);")
-#     phoneNumber = c.fetchone()
-#     print(phoneNumber)
-#     c.execute("SELECT * FROM Fares WHERE id=(SELECT max(id) FROM Fares);")
-#     fare = c.fetchone()
-#     print(fare)
-#     c.execute("SELECT * FROM Address WHERE id=(SELECT max(id) FROM Address);")
-#     address = c.fetchone()
-#     orderId = str(uuid.uuid4().fields[0])[:8]
-#     print(address)
-#     c.execute("INSERT INTO CreateOrder (phoneNumber, fare, address, orderId) VALUES (?, ?, ?, ?)", (str(phoneNumber[1]), str(fare[1]), str(address[1]), orderId, ))
-#     db.commit()
+
+
+
 
