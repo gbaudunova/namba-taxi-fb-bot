052e71be5697c39a88fb4f03df5bf68608c2c906
Gulbustan <gbaudunova@gmail.com>
2017-11-29 06:50:42 +0600

    Add some changes

--- modules/sendMessageButton.py
+++ modules/sendMessageButton.py
@@ -1,56 +1,33 @@
-import json
-import requests
-from bot import log
-from .messages import *
 from .sekret import *
-from .keyboard import getBasicKeyboardMessage
-from .needPhone import *
-from .needFares import createKeyboardFares
-from .db import insertFares
+from .messages import *
+from .keyboard import get_basic_keyboard_message
+from .keyboard import create_keyboard_fares
+from .db import insert_fares
+from .keyboard import send_button_message
 
 
-def decide_message(sender_id, message, data):
-    if(message == "call-taxi"):
+def decide_button(sender_id, message, data):
+    if message == "call-taxi":
         send_button_message(sender_id, PAGE_ACCESS_TOKEN, BOT_ASK_PHONE)
-    elif(message == 'rates'):
+    elif message == 'rates':
         send_button_message(sender_id, PAGE_ACCESS_TOKEN, BOT_BUTTON_MESSAGE2)
-    elif(message == 'getstarted'):
-        getBasicKeyboardMessage(sender_id)
-    elif(message == 'send-phone'):
+    elif message == 'getstarted':
+        get_basic_keyboard_message(sender_id)
+    elif message == 'save-phone':
         send_button_message(sender_id, PAGE_ACCESS_TOKEN, BOT_ASK_FARE)
-        createKeyboardFares(sender_id)
-    elif(message == 'order-status'):
+        create_keyboard_fares(sender_id)
+    elif message == 'order-status':
         send_button_message(sender_id, PAGE_ACCESS_TOKEN, BOT_MESSAGE_MY_ORDER_STATUS)
-    elif(message == 'nearest-drivers'):
+    elif message == 'nearest-drivers':
         send_button_message(sender_id, PAGE_ACCESS_TOKEN, BOT_MESSAGE_NEAREST_CARS)
-    elif(message == 'order-cancel'):
+    elif message == 'order-cancel':
         send_button_message(sender_id, PAGE_ACCESS_TOKEN, BOT_ORDER_CANCEL)
     else:
         callback = data['entry'][0]['messaging'][0]['postback']['payload']
-        insertFares(data)
+        insert_fares(data)
         send_button_message(sender_id, PAGE_ACCESS_TOKEN, BOT_ASK_ADDRESS)
 
 
-def send_button_message(sender_id, page_token, btn_message):
-    params = {
-        "access_token": page_token
-    }
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": sender_id
-        },
-        "message": {
-            "text": btn_message
-
-        }
-    })
-    r = requests.post(URL, params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
 
 
 
