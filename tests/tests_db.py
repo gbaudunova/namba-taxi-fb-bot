6dd079fa0dff8042f74c18d09e4dcf11a7433d7c
Gulbustan <gbaudunova@gmail.com>
2017-12-14 23:37:08 +0600

    add simple setup and teardown functions to the db tests

--- tests/tests_db.py
+++ tests/tests_db.py
@@ -1,7 +1,6 @@
 import os
 import sqlite3
 import unittest
-from .get_data import get_data
 
 
 class TestPhoneNumbersDb(unittest.TestCase):
@@ -10,10 +9,12 @@ class TestPhoneNumbersDb(unittest.TestCase):
         conn = sqlite3.connect('mydatabase.db')
         cursor = conn.cursor()
         variable = +996700080977
-        cursor.execute("INSERT INTO phone_number VALUES (NULL, ?)", (variable,))
+        cursor.execute("INSERT INTO phone_number VALUES (NULL, ?)",
+                       (variable,))
         cursor.execute("SELECT * FROM phone_number WHERE "
                        "id=(SELECT max(id) FROM phone_number);")
         phone_number = cursor.fetchone()
+        print(phone_number)
         conn.commit()
 
     def tearDown(self):
@@ -21,9 +22,3 @@ class TestPhoneNumbersDb(unittest.TestCase):
         Delete the database
         """
         os.remove("mydatabase.db")
-
-    def test_get_phone_number(self):
-        num = +996700080977
-        values = [(+996700080977, +996702124403)]
-        self.assertEqual(get_data(), values)
-
