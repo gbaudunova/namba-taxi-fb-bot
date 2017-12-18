1189fe81daf79a50cfc3d6aa6f69e3dfa62b50bd
Gulbustan <gbaudunova@gmail.com>
2017-12-18 21:15:51 +0600

    add test for get_address handler

--- tests/tests_db.py
+++ tests/tests_db.py
@@ -1,18 +1,25 @@
 import sqlite3
 import unittest
+from storage import get_data
 
 
 class TestPhoneNumbersDb(unittest.TestCase):
 
     @classmethod
-    def setUp(self):
+    def setUpClass(self):
         self.conn = sqlite3.connect('mydatabase.db')
         self.cursor = self.conn.cursor()
-        self.variable = +996700080977
-        self.cursor.execute("INSERT INTO phone_number VALUES (NULL, ?)",
-                            (self.variable,))
+        self.address = 'Исанова 4'
+        self.cursor.execute("INSERT INTO address VALUES (NULL, ?)",
+                            (self.address,))
         self.conn.commit()
 
     @classmethod
-    def tearDown(self):
+    def tearDownClass(self):
         pass
+
+    def test_get_phone_number(self):
+        self.cursor.execute("SELECT * FROM address WHERE"
+                            " id=(SELECT max(id) FROM address);")
+        last_address = self.cursor.fetchone()
+        self.assertEqual(get_data.get_address('mydatabase.db'), last_address)
