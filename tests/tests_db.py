80bcafbf7840ff366371898ade87ee2c4da35ba1
Gulbustan <gbaudunova@gmail.com>
2017-12-20 15:56:07 +0600

    set-up gitlab-ci file

--- tests/tests_db.py
+++ tests/tests_db.py
@@ -2,7 +2,6 @@ import sqlite3
 import unittest
 from storage import get_data
 from config import db_config
-from storage import db
 
 
 class TestPhoneNumbersDb(unittest.TestCase):
@@ -43,13 +42,15 @@ class TestPhoneNumbersDb(unittest.TestCase):
         self.cursor.execute("SELECT * FROM address WHERE \
                             id=(SELECT max(id) FROM address);")
         last_address = self.cursor.fetchone()
-        self.assertEqual(get_data.get_address(db_config.db_conf['name']), last_address)
+        self.assertEqual(get_data.get_address(db_config.db_conf['name']),
+                         last_address)
 
     def test_get_order_id(self):
         self.cursor.execute("SELECT * FROM order_id WHERE \
                             id=(SELECT max(id) FROM order_id);")
         order_id = self.cursor.fetchone()
-        self.assertEqual(get_data.get_order_id(db_config.db_conf['name']), order_id)
+        self.assertEqual(get_data.get_order_id(db_config.db_conf['name']),
+                         order_id)
 
     def test_delete_order_id(self):
         self.cursor.execute("SELECT * FROM order_id WHERE \
@@ -60,10 +61,3 @@ class TestPhoneNumbersDb(unittest.TestCase):
                               id=(SELECT max(id) FROM order_id);")
         order_id1 = self.cursor.fetchone()
         self.assertNotEqual(order_id, order_id1)
-
-    # def test_insert_phone_number(self):
-    #     number = {'entry': [{'messaging': [{'message': {'text': '+996702124404'}}]}]}
-    #     db.insert_phone_numbers(db_config.db_conf['name'], number)
-    #     self.cursor.execute("SELECT * FROM phone_number WHERE id=(SELECT max(id) FROM phone_number);")
-    #     result = self.cursor.fetchone()
-    #     self.assertEqual(number, result)
